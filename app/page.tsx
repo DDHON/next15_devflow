c0d7b0ec5bcccb73d1eb07c9a5391a2613864aca
Ramadhoni <ramadhonimuhammad612@gmail.com>
2024-10-30 22:09:17 +0800

    Tailwind Setup And Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl font-black text-white">
-      Welcome to the world Next.JS
-    </h1>
+    <>
+      <h1 className="text-3xl font-black text-light-500">
+        Welcome to the world Next.JS
+      </h1>
+    </>
   );
 };
 export default Home;
